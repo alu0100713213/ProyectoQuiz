c2c57ab6c3946361399744519050c3d8feba231f
Juan Ignacio Hita Manso <alu0100713213@ull.edu.es>
2016-01-20 20:12:50 +0000

    Arreglado y funcionando clase abstracta

--- models/abstract_quiz_model.js
+++ models/abstract_quiz_model.js
@@ -1,28 +1,15 @@
-var AbstractQuiz = require('../models/abstract_quiz_model.js');
-
-function Quiz(){
-
-	AbstractQuiz.call(this);
-	
-	this.q.push(
-	
-	{ pregunta: '¿Qué cantidad de huesos tiene en total el ser humano?,
-	  respuesta: function(x) {
-		return (206).exec(x);
-		}
-	}
-	);
-
-Quiz.prototype = new AbstractQuiz();
-Quiz.prototype.constructor = Quiz;
-	
-Quiz.prototype.numQuestions = function() {
-	return this.q.length;
+/* Monkey patching Math */
+Math.randomInt = function(x) {
+  return Math.floor(Math.random()*x);
 }
 
-Quiz.prototype.getQ = function(x){
-	return this.q[x]['pregunta'];
+function AbstractQuiz(q) {
+  this.q = q || [];
 }
 
-module.exports = Quiz;
+AbstractQuiz.prototype.randomQuestion = function() {
+  var index = Math.randomInt(this.q.length);
+  return this.q[index];
+}
 
+module.exports = AbstractQuiz;
