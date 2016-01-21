5f44b45cfea7274ab0e3c55ec97afebc11bcf14a
Juan Ignacio Hita Manso <alu0100713213@ull.edu.es>
2016-01-21 02:24:44 +0000

    Arreglando error en el controller

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,17 +1,18 @@
 var Quiz = require('../models/quiz_model');
-
 var quiz = new Quiz();
 var current = quiz.randomQuestion();
 
 exports.question = function(req,res) {
   current = quiz.randomQuestion();
-  res.render('quizes/question', {pregunta: current.pregunta});
+  current_new = quiz.getQuestion(current);
+  area = quiz.getArea(current);
+  res.render('quizes/question', {pregunta: current_new, area: area});
 };
 
 exports.answer = function(req, res) {
   var c = 'Incorrecto';
   if (current.respuesta(req.query.respuesta)) { c = 'Correcto'; }
-  res.render('quizes/answer', {respuesta: c})
+  res.render('quizes/answer', {respuesta: c});
 };
 
 exports.questions = function(req,res) {
@@ -21,7 +22,6 @@ exports.questions = function(req,res) {
   for(var i=0; i<nPreg; i++) {
     array[i] = (quiz.getQ(i));
   }
-
   res.render('quizes/questions', {prg: array})
 };
 
@@ -30,14 +30,15 @@ exports.specificQuestion = function(req, res) {
   var nPreg = quiz.numQuestions();
 
   if(id < 1 || id > nPreg){
-    res.render('quizes/SpecificQuestion', {prg: "No existe esa pregunta."})
+    res.render('quizes/SpecificQuestion', {prg: "La pregunta NO existe."})
   }
   else if(isNaN(id) === true) {
-    res.render('quizes/SpecificQuestion', {prg: "Error en la URL."})
+    res.render('quizes/SpecificQuestion', {prg: "URL Error"})
   }
   else {
     current = quiz.q[id-1];
-
-    res.render('quizes/question', {pregunta: current.pregunta});
+    current_new = quiz.getQuestion(current);
+    area = quiz.getArea(current);
+    res.render('quizes/question', {pregunta: current_new, area: area});
   }
 };
