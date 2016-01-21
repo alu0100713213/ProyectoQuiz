3fe0fbf0a55bca01e2412548affc1b849d5b671c
Juan Ignacio Hita Manso <alu0100713213@ull.edu.es>
2016-01-21 16:34:46 +0000

    Solucionado los errores, probando

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cooki
-e-parser');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var partials = require('express-partials');
 var methodOverride = require('method-override');
