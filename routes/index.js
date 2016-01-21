b312c7b44610ba01edeb7516b4f98e2e9f10c784
Juan Ignacio Hita Manso <alu0100713213@ull.edu.es>
2016-01-21 16:21:48 +0000

    Añadiendo las rutas para la sesión de usuarios

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -9,6 +10,14 @@ router.get('/', function(req, res) {
 });
 
 router.param('quizId', quizController.load); //QUIZ 10
+
+// Definicion de rutas de sesión
+
+router.get('/login', sessionController.new);
+router.post('/login', sessionController.create);
+router.get('/logout', sessionController.destroy);
+
+//Definicion de rutas para las preguntas 
 
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
