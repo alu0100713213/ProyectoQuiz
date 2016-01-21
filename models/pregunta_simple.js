8501aa6aeccd687adaa0a81b082817ed862b0b4f
Juan Ignacio Hita Manso <alu0100713213@ull.edu.es>
2016-01-21 03:21:09 +0000

    Creada la vista para seleccion simple

--- models/pregunta_simple.js
+++ models/pregunta_simple.js
@@ -1,10 +1,17 @@
 var Pregunta = require('../models/pregunta.js');
-
+var EJS = require('ejs');
 
 function SeleccionSimple(x, array){
 	Pregunta.call(this);
 	this.pregunta_ = x;
 	this.array_ = array;
+	this.area_;
+
+	var self=this;
+	EJS.renderFile('views/quizes/pregunta_simple.ejs',{array:this.array_}, function(err,html){
+	if(err)throw err;
+	else self.area_ = html;
+	});
 	
 /**	this.area = "<select name='respuesta[]'>";
 	for(var i=0; i<array.length; i++){
