e25c8724f90d8bf73b20d012de02073be646d3aa
Juan Ignacio Hita Manso <alu0100713213@ull.edu.es>
2016-01-21 13:05:12 +0000

    Modificando los modelos

--- models/models.js
+++ models/models.js
@@ -1,23 +1,49 @@
 var path = require('path');
-var Sequelize = require('sequelize');
-var sequelize = new Sequelize(null, null, null,
-	{
-		dialect: "sqlite",
-		storage: "quiz.sqlite"	
-	}
-);
+var databaseURL = process.env.DATABASE_URL          || 'sqlite://:@:/quiz';
+var databaseStorage  = process.env.DATABASE_STORAGE || 'quiz.sqlite';
+//                           1        2    3    4    5     6
+var url = databaseURL.match(/(.*):\/\/(.*):(.*)@(.*):(.*)\/(.*)/);
+var DB_name  = (url[6] || null); console.log("DB_name: "+DB_name);
+var user     = (url[2] || null); console.log("database user: "+user);
+var pwd      = (url[3] || null); console.log("password: "+pwd);
 
-var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+var protocol = (url[1] || null); console.log("protocol: "+protocol);
+var dialect  = protocol;         console.log("dialect: "+dialect);
+var port     = (url[5] || null); console.log("port: "+port);
+var host     = (url[4] || null); console.log("host: "+host);
+var storage  = databaseStorage;  console.log("database storage: "+storage);
+
+// Cargar modelo ORM
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('sqlite://@:/', { storage: storage });
+
+// Importar la definición de la tabla Quiz en quiz.js
+var quiz_path = path.join(__dirname, 'quiz');
+var Quiz = sequelize.import(quiz_path);
+
+// Exportar definición de la tabla Quiz
 exports.Quiz = Quiz;
 
-sequelize.sync().success(function(){
-	Quiz.count().success(function(count){
-	if(count === 0){
-		Quiz.create(
-		    { pregunta: "¿Qué cantidad de huesos tiene en total el ser humano?",
-      		      respuesta: "Seis"
-		     })
-		.success(function(){console.log("Base de datos correcta")});
-	};
-	 });
+sequelize.sync().then(function(){
+  Quiz.count().then(function(count) {
+    if (count === 0) { // La tabla se incializa si está vacía
+      Quiz.create({
+        pregunta: "Capital de Italia",
+        respuesta: "Roma"
+      });
+      Quiz.create({
+        pregunta: "Capital de España",
+        respuesta: "Madrid"
+      });
+      Quiz.create({
+        pregunta: "Capital de Portugal",
+        respuesta: "Lisboa"
+      }).then(function(){
+        console.log("Base de datos inicializada");
+      });
+    }
+  });
 });
+
+
+
